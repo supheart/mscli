cd5f7a553439a64cb2951a0a42b2bc270ad256e1
Milo Lin <milo@Milo-MacBook.local>
2018-10-17 15:30:24 +0800

    提交基本文件内容

---
+++ README.md
@@ -0,0 +1,37 @@
+[![npm package](https://img.shields.io/npm/v/mscli.svg)](https://www.npmjs.com/package/mscli)
+
+# 前端项目生成工具
+快速生成一个可用的前端项目的脚手架，包括express、koa、react、vue、angular，用于部分架构的补充和完善，方便平台模拟和调试开发。
+
+# 基本使用
+
+### 安装
+```shell
+npm install -g mscli
+# express 项目
+mscli express app-name
+# koa 项目
+# mscli koa app-name
+npm install
+npm start
+
+# 查看项目帮助
+mscli express -h
+```
+
+## express 项目:
+
+### 参数
+-p --page 是否是一个带页面的项目
+-u --upload 是否是一个带上传功能的项目
+-s --sql 是否是一个带sql查询的项目，这个现在只支持mysql
+
+### 使用
+
+```shell
+# 一个带页面但是没上传和sql的项目，如果默认不带参数是所有参数都带有的
+mscli express -p app-name
+```
+
+# License
+mscli is MIT licensed.
---
+++ bin/mscli.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const inquirer = require('inquirer');
+
+// 定义默认的命令
+program
+  .version('0.1.0', '-v, --version')
+
+// 定义二级命令
+program.command('express <app-name>')
+    .description('create a new project by express')
+    .option('-p, --page', 'is a page project')
+    .option('-s, --sql', 'is a sql project')
+    .option('-u, --upload', 'is a upload project')
+    .action(async (name, cmd) => {
+        const options = await getOptionsByCmd(cmd);
+        require('../lib/express')(name, options);
+    });
+
+program.command('koa <app-name>')
+    .description('create a new project by koa')
+    .option('-a, --page', 'is a page project')
+    .option('-s, --sql', 'is a sql project')
+    .option('-u, --upload', 'is a upload project')
+    .action(async (name, cmd) => {
+        const options = await getOptionsByCmd(cmd);
+        console.log(options, 666);
+    });
+
+async function getOptionsByCmd(cmd) {
+    let options = {};
+    let { page, sql, upload } = cmd;
+    if (!cmd.page && !cmd.sql && !cmd.upload) {
+        options = await inquirer.prompt([{
+            name: 'ok',
+            type: 'confirm',
+            message: `is show option choose，default is no, using empyt option?`,
+            default: false
+        }]).then(result => {
+            if( result.ok) {
+                return inquirer.prompt([{
+                    name: 'page',
+                    type: 'confirm',
+                    message: `is a page project?`,
+                    default: false
+                }]);
+            }
+            return { page: true, sql: true, upload: true };
+        }).then(result => {
+            if(result.page && result.sql && result.upload ) return result;
+            page = result.page;
+            if(result.page) {
+                return inquirer.prompt([{
+                    name: 'upload',
+                    type: 'confirm',
+                    message: `is a upload project?`,
+                    default: false
+                }, {
+                    name: 'sql',
+                    type: 'confirm',
+                    message: `is a sql project?`,
+                    default: false
+                }]);
+            } else {
+                return inquirer.prompt([{
+                    name: 'sql',
+                    type: 'confirm',
+                    message: `is a sql project?`,
+                    default: false
+                }]);
+            }
+        }).then(result => {
+            return { page, upload, sql, ...result };
+        }).catch((err) => {
+            console.log(err);
+            return { page, sql, upload }
+        });
+    } else {
+        options = { page, sql, upload };
+    }
+    return options;
+}
+
+// 实现参数监听
+program.parse(process.argv);
